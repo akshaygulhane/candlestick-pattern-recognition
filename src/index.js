9e7eaaaf223e975c91b76e39e23fc80b888a1dd4
Akshay Gulhane <akshay@proxce.com>
2018-10-31 16:52:19 +0530

    Added console message

--- src/index.js
+++ src/index.js
@@ -8,13 +8,16 @@ module.exports = {
     initialize_app: async function (INPUT_CSV_PATH, OUTPUT_CSV_PATH) {
 
         try {
+            console.log("Starting the app..")
             let raw_data = fs.readFileSync(INPUT_CSV_PATH, 'utf-8');
 
             let parsed_data = await parse_data(raw_data);
 
             let hammer_data = await find_hammer_data(parsed_data)
-            console.log(hammer_data)
-            write_csv(OUTPUT_CSV_PATH, hammer_data)
+
+            write_csv(OUTPUT_CSV_PATH, hammer_data);
+
+            console.log(`CandleStick data written to the output file at : ${OUTPUT_CSV_PATH}`)
 
         }
         catch (err) {
